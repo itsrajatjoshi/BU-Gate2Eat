3a63ff13eefdf4b617f6fac1a75f504ee280f8eb
Rajat Joshi <itsrajatjoshi@gmail.com>
2026-08-03 21:42:40 +0530

    chore: clean unused import in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'core/constants/app_constants.dart';
 import 'core/providers.dart';
